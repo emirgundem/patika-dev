84eff0524ac4ddd4cf4674fa2b70f18b354d703a
emirgundem <emirgndem@gmail.com>
2022-04-18 12:35:50 +0300

    web server

--- web-server/server.js
+++ web-server/server.js
@@ -1,40 +1,34 @@
 const http = require('http');
-const port = 3000;
+const port = 5000;
 
 const server = http.createServer((req,res)=>{
-   
-    const url = req.url;
-    console.log(url);
+     
+     const url = req.url
 
-    if(url==='/'){
-        res.writeHead(200,{"Content-Type":"text/html"});
-        res.write("<h1>Home Page</h1>")
-    }
+     if(url === '/index'){
+         res.writeHead(200, { "Content-Type": "text/html" });
+         res.write('<h2>Index Sayfasına Hoşgeldiniz</h2>');
+     }
 
-    else if(url==='/about'){
-        res.writeHead(200,{"Content-Type":"text/html"});
-        res.write("<h1>About Page</h1>")
-    }
+     else if(url==='/hakkımda'){
+         res.writeHead(200, { "Content-Type": "text/html" });
+         res.write('<h2>Hakkımda Sayfası</h2>');
+     }
 
-    else if(url==='/contact'){
-        res.writeHead(200,{"Content-Type":"text/html"});
-        res.write("<h1>Contact Page</h1>")
-    }
-    
-    else {
-       
-        res.writeHead(404,{"Content-Type":"text/html"});
-        res.write("<h1>Contact Page</h1>")
-    }
+     else if(url==='/iletisim'){
+         res.writeHead(200, { "Content-Type": "text/html" });
+         res.write('<h2>İletisim Page<h2/>');
+     }
 
-
-    res.end();
+     else {
+         res.writeHead(404, { "Content-Type": "text/html" });
+         res.write('Sayfa Bulunamadı');
+     }
+     res.end();
 })
 
 
-
-server.listen(port,() =>{
-    console.log(`Server running on port :  ${port}`);
-});
-
+server.listen(port,()=>{
+    console.log(`Server running on port ${port}`);
+})
 
